e27c6685c2d9cbe241af85d8af26df2a3988276f
canonnizq <canonnizq@gmail.com>
2024-10-27 15:56:17 +0800

    create namespace

---
+++ lib/routes/theguardian/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from "@/types";
+
+export const namespace: Namespace = {
+    name: 'The Guardian',
+    url: 'theguardian.com',
+    zh: {
+        name: '卫报'
+    }
+}
+
