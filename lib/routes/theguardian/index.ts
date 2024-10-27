e87d276ecc3c4606b31348af56ba17037fa360e1
canonnizq <canonnizq@gmail.com>
2024-10-27 21:32:34 +0800

    fix index

--- lib/routes/theguardian/index.ts
+++ lib/routes/theguardian/index.ts
@@ -19,15 +19,22 @@ async function handler(ctx) {
     const { edition = 'international' } = ctx.req.param();
 
     title = `The Guardian ${edition.toUpperCase()}`;
-    feed = await ()
+    feed = await (parser.parseURL(`https://www.theguardian.com/${edition}/rss`));
 
     const items = await Promise.all(
-        list.map((item) => 
+        feed.items.map((item) => 
             cache.tryGet(item.link, async () => {
                 const response = await ofetch(`https://www.theguardian.com/${edition}`);
                 const $ = load(response);
 
-                return item;
+                let description = item.content;
+
+                return {
+                    title: item.title,
+                    description,
+                    pubDate: item.pubDate,
+                    link: item.link
+                };
             })
         )
     );
